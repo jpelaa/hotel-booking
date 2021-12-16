dda52ec8d6f533007469f935f4f42bb496a9073c
Jayaprakash Elavarasan <jayaprakash_elavarasn@trimble.com>
2021-12-16 23:24:44 +0530

    navbar spacing

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,14 @@ import { Box, Flex, Heading } from "@chakra-ui/react";
 
 const NavBar = () => {
   return (
-    <Box w="100%" h="50px" bgGradient="linear(to-r, primary, secondary)">
+    <Box
+      mx="4"
+      my="2"
+      h="50px"
+      borderRadius="base"
+      px="2"
+      bgGradient="linear(to-r, primary, secondary)"
+    >
       <Flex justifyContent="space-between">
         <Box>
           <Heading>Logo</Heading>
